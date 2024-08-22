12b0062fae9dc1eea454dd69d8de075f82f0fba1
Montenegro Matias <matihandball08@gmail.com>
2024-08-22 17:19:27 -0300

    cambio de banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import MetodosPago from './MetodosPago';
-import banner from '/img/bannersuperior1.png'
+import banner from '/img/bannersuperior1.svg'
 import './home.css';
 import { Link } from 'react-router-dom';
 
